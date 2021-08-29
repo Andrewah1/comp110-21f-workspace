2335141c2bdbcdc4b82f9aa7f4f0de2cc284c537
Andrew <andrewah@email.unc.edu>
2021-08-28 21:16:53 -0400

    fixed spacing

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -5,7 +5,7 @@ __author__ = "730389123"
 Left_hand_side = int(input("Left-hand side: "))
 Right_hand_side = int(input("Right-hand side: "))
 
-print(Left_hand_side, " < ", Right_hand_side, " is ", (Left_hand_side < Right_hand_side))
-print(Left_hand_side, " >= ", Right_hand_side, " is ", (Left_hand_side >= Right_hand_side))
-print(Left_hand_side, " == ", Right_hand_side, " is ", (Left_hand_side == Right_hand_side))
-print(Left_hand_side, " != ", Right_hand_side, " is ", (Left_hand_side != Right_hand_side))
+print(Left_hand_side, "<", Right_hand_side, "is", (Left_hand_side < Right_hand_side))
+print(Left_hand_side, ">=", Right_hand_side, "is", (Left_hand_side >= Right_hand_side))
+print(Left_hand_side, "==", Right_hand_side, "is", (Left_hand_side == Right_hand_side))
+print(Left_hand_side, "!=", Right_hand_side, "is", (Left_hand_side != Right_hand_side))
