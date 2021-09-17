9970dc68be78b2a7084c9a88b64b51850612a937
Andrew <andrewah@email.unc.edu>
2021-09-16 22:26:46 -0400

    homework

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,22 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730389123"
+
+word = str(input("Enter a word: "))
+dup: bool = False 
+
+i: int = 0
+while i < len(word):
+    char = str(word[i])
+    j: int = i + 1
+    while j < len(word):
+        test = str(word[j])
+        if char == test:
+            dup: bool = True
+        j = j + 1
+    i = i + 1
+
+if dup == False:
+    print("Found duplicate: False")
+else:
+    print("Duplicate found: True")
