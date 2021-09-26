c86830e11b032d62cff2733b1f98739a7dcff3fb
Andrew <andrewah@email.unc.edu>
2021-09-26 16:18:52 -0400

    homework 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,13 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730389123"
 
 
-# TODO: Implement your functions here.
+def all(x: list[int], y: int) -> bool:
+    i: int = 0
+    while i < len(x):
+        z: int = x[i]
+        if z != y:
+            return False
+        i += 1
+    return True
