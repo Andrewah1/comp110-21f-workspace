4394c5b517a68d35e456f75742622ad7a48300ce
Andrew <andrewah@email.unc.edu>
2021-09-26 21:35:33 -0400

    ex04 first try

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -11,3 +11,22 @@ def all(x: list[int], y: int) -> bool:
             return False
         i += 1
     return True
+
+
+def is_equal(x: list[int], y: list[int]) -> bool:
+    if x == y:
+        return True
+    else:
+        return False
+
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    while i < len(input):
+        x: int = input[i]
+        y: int = input[i + 1]
+        if x >= y:
+            return x
+        i += 1
