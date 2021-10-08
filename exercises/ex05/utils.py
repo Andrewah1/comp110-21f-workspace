7110d174c28e8ba147901a86fb63f5508f447e5e
Andrew <andrewah@email.unc.edu>
2021-10-07 22:13:14 -0400

    EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,39 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730389123"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    """Pulls only even numbers from list."""
+    y: list[int] = list()
+    i: int = 0
+    while i < len(x):
+        if x[i] % 2 == 0:
+            y.append(x[i])
+        i += 1
+    return y
+
+
+def sub(x: list[int], y: int, z: int) -> list[int]:
+    """Creates a sublist of a master list limited to specific integers."""
+    a: list[int] = list()
+    i: int = 0
+    while i < len(x):
+        if x[i] > y and x[i] <= z:
+            a.append(x[i])
+        i += 1
+    return a
+
+
+def concat(x: list[int], y: list[int]) -> list[int]:
+    """Combines two lists."""
+    z: list[int] = list()
+    i: int = 0
+    j: int = 0
+    while i < len(x):
+        z.append(x[i])
+        i += 1 
+    while j < len(y):
+        z.append(y[j])
+        j += 1
+    return z
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,69 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730389123"
+
+
+def test_only_evens_empty() -> None:
+    """Edge case test."""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_one() -> None:
+    """Generel test one."""
+    xs: list[int] = [5, 6, 7, 8, 9, 10]
+    assert only_evens(xs) == [6, 8, 10]
+
+
+def test_only_evens_two() -> None:
+    """General test two."""
+    xs: list[int] = [25, 70, 110, 1000, 2577]
+    assert only_evens(xs) == [70, 110, 1000]
+
+
+def test_sub_empty() -> None:
+    """Edge case test."""
+    xs: list[int] = []
+    j: int = 50
+    k: int = 10000
+    assert sub(xs, j, k) == []
+
+
+def test_sub_one() -> None:
+    """Generel test one."""
+    xs: list[int] = [10, 60, 5000, 10000, 25, 100]
+    j: int = 50
+    k: int = 10000
+    assert sub(xs, j, k) == [60, 5000, 10000, 100]
+
+
+def test_sub_two() -> None:
+    """General test two."""
+    xs: list[int] = [-1, 15, 76, 99]
+    j: int = -10
+    k: int = 100
+    assert sub(xs, j, k) == [-1, 15, 76, 99]
+
+
+def test_concat_empty() -> None:
+    """Edge case test."""
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_one() -> None:
+    """Generel test one."""
+    xs: list[int] = [15, 16, 17, 18]
+    ys: list[int] = [20, 30, 50]
+    assert concat(xs, ys) == [15, 16, 17, 18, 20, 30, 50]
+
+
+def test_concat_two() -> None:
+    """General test two."""
+    xs: list[int] = [-10, 50, 100]
+    ys: list[int] = [-100, 75, 10000]
+    assert concat(xs, ys) == [-10, 50, 100, -100, 75, 10000]
