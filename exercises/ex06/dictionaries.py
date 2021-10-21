bbc0ef65ba2e1c4aed047a141706747947036b1f
Andrew <andrewah@email.unc.edu>
2021-10-20 22:57:41 -0400

    ex06 draft

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,56 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730389123"
 
-# Define your functions below
+
+def main() -> None:
+    print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
+
+
+def invert(original: dict[str, str]) -> dict[str, str]:
+    """Invert a dictionary."""
+    invert: dict[str, str] = dict()
+    x: str = " "
+    for key in original:
+        x = original[key]
+        invert[x] = key
+    return invert
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Returns most common color in a dictionary."""
+    color: str = " "
+    i: int = 0
+    color_list: list[str] = list()
+    for key in colors:
+        color_list.append(colors[key])
+    j: int = 0
+    while j < len(color_list):
+        k: int = 0
+        ct: int = 0
+        y: str = color_list[j]
+        while k < len(color_list):
+            if color_list[k] == ct:
+                ct += 1
+            k += 1
+        if i < ct:
+            color = y
+            i = ct
+        j += 1
+    return color
+
+
+def count(content: list[str]) -> dict[str, int]:
+    """Returns dictionary given a list with list and number of times an item appears in the list."""
+    count: dict[str, int] = dict()
+    i: int = 0
+    while i < len(content):
+        if content[i] in count:
+            count[content[i]] += 1
+        else:
+            count[content[i]] = 1
+    return count
+
+
+if __name__ == "__main__":
+    main()
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,54 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730389123"
+
+
+def test_invert_empty() -> None:
+    """Edge Case Test."""
+    xs: dict[str, str] = dict()
+    assert invert(xs) == {}
+
+
+def test_invert_one() -> None:
+    """Test One."""
+    xs: dict[str, str] = {'1': '2', '5': '6'}
+    assert invert(xs) == {'2': '1', '6': '5'}
+
+
+def test_invert_two() -> None:
+    """Test Two."""
+    xs: dict[str, str] = {'john': 'Steeve', 'Table': 'Chair', 'up': 'down', 'left': 'right'}
+    assert invert(xs) == {'Steeve': 'john', 'Chair': 'Table', 'down': 'up', 'right': 'left'}
+
+
+def test_favorite_color_empty() -> None:
+    xs: dict[str, str] = dict()
+    assert favorite_color(xs) == ""
+
+
+def test_favorite_color_one() -> None:
+    xs: dict[str, str] = {'john': 'red', 'jack': ' blue', 'Macy': 'red'}
+    assert favorite_color(xs) == "red"
+
+
+def test_favorite_color_two() -> None:
+    xs: dict[str, str] = {'john': 'red', 'jack': ' blue', 'Macy': 'red', 'Brandon': 'green', 'steve': 'green'}
+    assert favorite_color(xs) == "red"
+
+
+def test_count_empty() -> None:
+    xs: list[str] = list()
+    assert count(xs) == {}
+
+
+def test_count_one() -> None:
+    xs: list[str] = ["chair", "chair", "sky", "table", "ground"]
+    assert count(xs) == {'chair': 2, 'sky': 1, 'table': 1, 'ground': 1}
+
+
+def test_count_two() -> None:
+    xs: list[str] = ["left", "left", "left", "left", "left", "left", "right"]
+    assert count(xs) == {'left': 5, 'right': 1}
